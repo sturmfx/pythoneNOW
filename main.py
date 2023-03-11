1e88f5967b4ecbbc5c8d302b8f2fe2f3d8a7d373
sturmfx <ttunikitashtobert@gmail.com>
2023-03-11 15:47:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,27 @@
-import time
 import tkinter as tk
-import tkinter.colorchooser as colorchooser
 import random
 
 root = tk.Tk()
-root.title("DRAWING CIRCLES")
-
+root.title("TEST")
 canvas = tk.Canvas(root, width=500, height=500)
-size = tk.Entry(root)
 
-color1 = '#F0F8FF'
+colors = ['#37AB65', '#3DF735', '#AD6D70', '#EC2504', '#8C0B90']
+circle_size_max = 100
+circle_size_min = 20
 
 
-def choose_color():
-    global color1
-    color_c = colorchooser.askcolor(title="CHOOSE COLOR")[1]
-    if color_c:
-        color1 = color_c
+def draw_random_circle(event):
+    global circle_size_min
+    global circle_size_max
+    circle_size = random.randint(circle_size_min, circle_size_max)
+    x = random.randint(circle_size, 500 - circle_size)
+    y = random.randint(circle_size, 500 - circle_size)
+    color = random.choice(colors)
+    canvas.create_oval(x - circle_size, y - circle_size, x + circle_size, y + circle_size, fill=color)
     canvas.focus_set()
 
 
-def draw(event):
-    circle_size = int(size.get())
-    x = event.x
-    y = event.y
-    item = canvas.create_oval(x - circle_size, y - circle_size, x + circle_size, y + circle_size, fill=color1)
-    return item
-
-
-def draw2(event):
-    circle_size = int(size.get())
-    x = event.x
-    y = event.y
-    item = canvas.create_rectangle(x - circle_size / 2, y - circle_size / 2, x + circle_size / 2, y + circle_size / 2,
-                                   fill=color1)
-    return item
-
-
-def draw3(event):
-    circle_size = int(size.get())
-    x = event.x
-    y = event.y
-    item = canvas.create_polygon(x, y - circle_size, x - circle_size, y + circle_size, x + circle_size, y + circle_size,
-                                 fill=color1, outline='#000000')
-    return item
-
-
-def clear(event):
-    canvas.delete("all")
-
-
-def increase(event):
-    current = int(size.get())
-    size.delete(0, tk.END)
-    size.insert(0, str(current + 1))
-
-
-def decrease(event):
-    current = int(size.get())
-    if current > 1:
-        size.delete(0, tk.END)
-        size.insert(0, str(current - 1))
-
-
-def random_color(event):
-    global color1
-    color1 = '#' + ''.join(random.choices('1234567890ABCDEF', k=6))
-    canvas.focus_set()
-
-
-def move(item):
-    x1 = random.randint(0, 500)
-    y1 = random.randint(0, 500)
-
-    x2 = random.randint(0, 500)
-    y2 = random.randint(0, 500)
-
-    dx = (x2 - x1) / 50
-    dy = (y2 - y1) / 50
-
-    for i in range(50):
-        canvas.move(item, dx, dy)
-        root.update()
-        time.sleep(0.05)
-
-
-color = tk.Button(root, text="CHOOSE COLOR", command=choose_color)
-canvas.bind("<Button-1>", lambda event: move(draw(event)))
-canvas.bind("<Button-2>", lambda event: move(draw3(event)))
-canvas.bind("<Button-3>", lambda event: move(draw2(event)))
-canvas.bind("<Key-r>", clear)
-canvas.bind("<Key-minus>", decrease)
-canvas.bind("<Key-plus>", increase)
-canvas.bind("<Key-f>", random_color)
+canvas.bind("<Key-r>", draw_random_circle)
 canvas.pack()
-size.pack()
-color.pack()
+canvas.focus_set()
 root.mainloop()
